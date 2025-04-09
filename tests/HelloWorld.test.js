639c088cb80f8de23f028917badbec656fdb21cc
test <brucemclarenp4@gmail.com>
2025-04-10 00:56:17 +0900

    不要テストケース削除

--- tests/HelloWorld.test.js
+++
@@ -1,7 +0,0 @@
-import { render, screen } from '@testing-library/vue'
-import HelloWorld from '@/components/HelloWorld.vue'
-
-test('名前が表示されること', () => {
-  render(HelloWorld, { props: { name: 'Taro' } })
-  expect(screen.getByText('Hello Taro')).toBeTruthy()
-})
