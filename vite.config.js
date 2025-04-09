5b4454bdabec740d72ca2c677088568dd5faea2b
test <brucemclarenp4@gmail.com>
2025-04-10 00:53:41 +0900

    Vitest導入

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,10 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [vue(),tailwindcss()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"), 
