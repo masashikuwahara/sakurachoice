478f2a57ced732c5fb7a0fafc81f141bb9ba98bd
test <brucemclarenp4@gmail.com>
2025-04-19 03:49:56 +0900

    トップページ画像削除

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,3 +1,24 @@
+<template>
+  <div class="min-h-screen flex flex-col justify-center items-center bg-gradient-to-br from-[#ffffff] to-[#f19db5] p-4">
+    <h1 class="text-3xl sm:text-5xl font-bold text-gray-800 mb-8 text-center">
+      SAKURACHOICE
+    </h1>
+    <P>櫻坂46の推しメンチェッカーです</P>
+    <!-- <img src="https://www.crank-in.net/img/db/243113240232456_1200.jpg" alt="" class="hinata"><br> -->
+    <button
+      @click="startComparison"
+      class="bg-white border border-[#e84a79] text-[#e84a79] hover:bg-[#ffe4ec] text-lg sm:text-xl font-semibold py-3 px-6 rounded-xl shadow-md transition transform hover:scale-105"
+    >
+      スタート
+    </button><br>
+    -更新履歴-
+    <div v-for="history in histories">
+      {{ history.date }} {{ history.content }}
+    </div>
+    1.0.0-rc1
+  </div>
+</template>
+
 <script setup>
 import { onMounted } from "vue";
 import { useItemStore } from "@/stores/items";
@@ -16,28 +37,8 @@ const startComparison = () => {
 };
 
 const histories = ref([
+  { date: '2025.04.19', content: 'RC版' },
   { date: '2025.04.10', content: 'ベータ2版' },
   { date: '2025.04.08', content: 'ベータ版' }
 ])
 </script>
-
-<template>
-  <div class="min-h-screen flex flex-col justify-center items-center bg-gradient-to-br from-[#ffffff] to-[#f19db5] p-4">
-    <h1 class="text-3xl sm:text-5xl font-bold text-gray-800 mb-8 text-center">
-      SAKURACHOICE
-    </h1>
-    <P>櫻坂46四期生の推しメンチェッカーです</P>
-    <img src="https://www.crank-in.net/img/db/243113240232456_1200.jpg" alt="" class="hinata"><br>
-    <button
-      @click="startComparison"
-      class="bg-white border border-[#e84a79] text-[#e84a79] hover:bg-[#ffe4ec] text-lg sm:text-xl font-semibold py-3 px-6 rounded-xl shadow-md transition transform hover:scale-105"
-    >
-      スタート
-    </button><br>
-    -更新履歴-
-    <div v-for="history in histories">
-      {{ history.date }} {{ history.content }}
-    </div>
-    1.0.1-beta.2
-  </div>
-</template>
